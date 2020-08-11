ad968a1573bc590d6ef355581e8d208d44cf7d09
unknown <prasadnithi15@gmail.com>
2020-08-11 23:43:16 +0530

    registration form front end

---
+++ Banking-app/online-banking-app/src/app/registration.ts
@@ -0,0 +1,11 @@
+import { logging } from 'protractor';
+
+export class Registration{
+    id:number;
+    username:string;
+    firstname:string;
+    lastname:string;
+    email:string;
+    phonenumber:number;
+    password:string;
+}
